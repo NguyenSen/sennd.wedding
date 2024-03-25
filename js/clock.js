408f56d407d281374d068321bd2d8d8b2cd59034
sennd <sennd@vietmap.vn>
2024-03-25 17:09:47 +0700

    fix timezone and spell

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     let currentDate = new Date();
 
     // Target future date/24 hour time/Timezone
-    let targetDate = moment.tz("2024-04-14 12:00", "Asia/Jarkarta");
+    let targetDate = moment.tz("2024-04-14 12:00", "Asia/Jakarta");
 
     // Calculate the difference in seconds between the future and current date
     let diff = targetDate / 1000 - currentDate.getTime() / 1000;
